aac54565818997a89042f5ca390bf18b51fdbfee
Bob131 <bob@jiyuulinux.org>
2013-12-30 21:48:10 +1100

    typo fix

--- modules/stats.py
+++ modules/stats.py
@@ -2,7 +2,7 @@
 def stats(self, cmd):
     import datetime
     statsdict = self.conman.mpc.stats()
-    self.conman.privmsg("Uptime: %s - Playtime: %s" % (datetime.timedelta(seconds=int(statsdict["uptime"]), datetime.timedelta(seconds=int(statsdict["playtime"])))))
+    self.conman.privmsg("Uptime: %s - Playtime: %s" % (datetime.timedelta(seconds=int(statsdict["uptime"])), datetime.timedelta(seconds=int(statsdict["playtime"]))))
     self.conman.privmsg("Artists: %s - Albums: %s - Songs: %s" % (statsdict["artists"], statsdict["albums"], statsdict["songs"]))
 
 #Maps command and help text for command
