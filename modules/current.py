4de5cf5acfecec09512c282d4283fafdcfc6a697
Bob131 <bob@jiyuulinux.org>
2014-01-07 06:20:25 +1100

    Fix 2

--- modules/current.py
+++ modules/current.py
@@ -1,6 +1,8 @@
 def current(self, lel):
     self.require("format_song_details")
     currentTrack = self.conman.mpc.currentsong()
+    if type(currentTrack == list):
+        currentTrack = currentTrack[0]
     tosend = self.run_func("format_song_details", [currentTrack["file"]])
     if thread_types[threading.current_thread().ident] == "HTTP":
         import math
